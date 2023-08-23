6709f6ac8181fbd46d44139cbeb120c0581b8751
victshev <victshev259@gmail.com>
2023-08-23 18:48:58 +0400

    Testadded

---
+++ src/test/java/ru/netology/PostmanTest.java
@@ -0,0 +1,30 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
+
+class requestPostman {
+
+    @Test
+    void shouldPostRequestPostman() {
+        // Given - When - Then
+        // Предусловия
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Привет!") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Привет!"));
+
+    }
+}
